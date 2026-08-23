d407b6bb94d1b923910ac9e322905c4a4822863c
Olof Hellman <olof@mac.com>
2026-08-23 12:58:19 -0700

    Conform SAEDocument to SAEClass

--- Sources/SinterAppleEvents/SAEDocument.swift
+++ Sources/SinterAppleEvents/SAEDocument.swift
@@ -8,6 +8,10 @@
 
 import Foundation
  
-open class SAEDocument: SAEObject {
+open class SAEDocument: SAEObject, SAEClass {
     public static let fcc: FourCharCode = .classDocument
+    
+    required override public init(app: SAEApp?, objSpec: NSAppleEventDescriptor) {
+        super.init(app: app, objSpec: objSpec)
+    }
 }
